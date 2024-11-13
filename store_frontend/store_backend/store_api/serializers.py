fb5c09fa4465e93fb4f16bba2203d299de50341a
abdullaheskeif <iabdullaheskeif@gmail.com>
2024-11-13 09:49:58 -0800

    version 0.002

--- store_frontend/store_backend/store_api/serializers.py
+++ store_frontend/store_backend/store_api/serializers.py
@@ -2,12 +2,13 @@ from rest_framework import serializers
 from store.models import Product
 from django.conf import settings
 
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Product
-        fields=('id','category','name','description','price','stock_quantity','stock_quantity','sku','created_at',
-                'updated_at','availability')
-        
+        model = Product
+        fields = ('id', 'category', 'name', 'description', 'price', 'stock_quantity', 'stock_quantity', 'sku', 'created_at',
+                  'updated_at', 'availability', 'images')
+
 
 class UserRegisterSerializer(serializers.ModelSerializer):
 
