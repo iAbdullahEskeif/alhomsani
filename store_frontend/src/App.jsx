6a9c6f8aba75bba13cb7c2730ec24fe4a79b49fb
abdullah eskeif <abdullah.m.eskef@gmail.com>
2025-03-21 16:57:15 +0300

    stupid bad

--- store_frontend/src/App.jsx
+++ store_frontend/src/App.jsx
@@ -1,38 +1,30 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar                           from "./components/Navbar";
-import Home                             from "./pages/Home";
-import About                            from "./pages/About";
-import Contact                          from "./pages/Contact";
-import ClassicCars                      from "./pages/ClassicCars";
-import LuxuryCars                       from "./pages/LuxuryCars";
-import ElectricalCars                   from "./pages/ElectricalCars";
-import SignUpModal                      from "./components/SignupModal.jsx"
-import AuthProvider from "react-auth-kit";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import ClassicCars from "./pages/ClassicCars";
+import LuxuryCars from "./pages/LuxuryCars";
+import ElectricalCars from "./pages/ElectricalCars";
+import SignUpModal from "./components/SignupModal.jsx"
 
 function App() {
-  return (
+    return (
 
-    <AuthProvider
-    authType={'localstorage'} // or 'sessionstorage' / 'cookie'
-    authName={'_auth'}
-    cookieDomain={window.location.hostname}
-    cookieSecure={false}>
+        <BrowserRouter>
 
-    <BrowserRouter>
-
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUpModal />} />
-        <Route path="/classiccars" element={<ClassicCars />} />
-        <Route path="/electricalcars" element={<ElectricalCars />} />
-        <Route path="/luxurycars" element={<LuxuryCars />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-    </AuthProvider> 
-  );
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/signup" element={<SignUpModal />} />
+                <Route path="/classiccars" element={<ClassicCars />} />
+                <Route path="/electricalcars" element={<ElectricalCars />} />
+                <Route path="/luxurycars" element={<LuxuryCars />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
