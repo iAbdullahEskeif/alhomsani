d38e0e67c411d87a3efad9941da0c6698529d331
Ali <alimartakoush@gmail.com>
2025-03-28 17:31:00 -0400

    Added clerk middleware in users.auth and modified settings

--- store_backend/core/urls.py
+++ store_backend/core/urls.py
@@ -11,6 +11,4 @@ urlpatterns = [
     path('api/', include('store_api.urls', namespace='store_api')),
     path('api/users/', include('users.urls', namespace='users')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
