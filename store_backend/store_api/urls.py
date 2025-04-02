cfe7d1ad81d3d378a7421d4f67f637293e1e24de
Ali <alimartakoush@gmail.com>
2025-04-02 15:46:54 -0400

    Modified Models.py to handle car_type, created an endpint and modified the serializer accordingly

--- store_backend/store_api/urls.py
+++ store_backend/store_api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import ProductDetail,ProductList
+from .views import ProductDetail,ProductList,FilteredProductListView
 from django.conf import settings
 from django.conf.urls.static import static
 app_name='store_post'
 urlpatterns=[
     path('<int:pk>/',ProductDetail.as_view(),name='detailcreate'),
-    path('',ProductList.as_view(),name='listcreate'),  
+    path('',ProductList.as_view(),name='listcreate'), 
+    path('filtered/',FilteredProductListView.as_view(),name='filtered_view') 
 
 
 
