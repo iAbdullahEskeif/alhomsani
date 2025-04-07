13d65216b50dc47e0f22dae0f873868f46e5d1bb
Ali <alimartakoush@gmail.com>
2025-04-07 12:33:41 -0400

    Test

--- store_backend/core/settings.py
+++ store_backend/core/settings.py
@@ -191,3 +191,4 @@ EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+#flag
--- store_backend/testing.py
+++ store_backend/testing.py
@@ -1,4 +1,5 @@
 def test(num1,num2):
+    flag=num1*num2
     return num1
 
 kwargs={"num1":1,"num2":2}
